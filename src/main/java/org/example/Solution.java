681dfda2acdd6cd7e65f7838579db56fdc74a6a2
iksadNorth <iksadNorth@gmail.com>
2023-08-30 09:13:20 +0900

    문제 풀이

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,8 +1,23 @@
 package org.example;
 
-public class Solution {
-    public String solution(String s) {
-        String answer = "";
-        return answer;
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 1;
+        for(int i : arr) {
+            answer = lcm(answer, i);
+        } return answer;
+    }
+
+    int lcm(int a, int b) {
+        return a * b / gcd(a,b);
+    }
+
+    int gcd(int a, int b) {
+        int x=Math.max(a,b), y=Math.min(a,b);
+
+        while(y!=0) {
+            int temp = x%y;
+            x = y; y = temp;
+        } return x;
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -8,20 +8,29 @@ import org.junit.jupiter.api.Test;
 class SolutionTest {
     private final Solution solution = new Solution();
 
-    public void assertAnswer(String s, String answer) {
+    public void assertAnswer(int[] s, int answer) {
         // when
-        String result = solution.solution(s);
+        int result = solution.solution(s);
 
         // then
         Assertions.assertEquals(result, answer);
     }
 
-    @Disabled
-    @Test @DisplayName("이 메서드는 Mock Up 이다.")
-    void testcase0() {
+    @Test @DisplayName("1")
+    void testcase1() {
         // given
-        String s = "1 2 3 4";
-        String answer = "1 4";
+        int[] s = {2,6,8,14};
+        int answer = 168;
+
+        // when & then
+        assertAnswer(s, answer);
+    }
+
+    @Test @DisplayName("2")
+    void testcase2() {
+        // given
+        int[] s = {1,2,3};
+        int answer = 6;
 
         // when & then
         assertAnswer(s, answer);
